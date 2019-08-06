91d4aabc072a752a378c1cda65f405b7e614264a
Bohdan Denysiuk <kotuchi0203@gmail.com>
2019-08-06 18:01:49 +0300

    add favicon

--- dest/js/main.js
+++ dest/js/main.js
@@ -8,7 +8,7 @@ var typedString = document.querySelector(".was-sent");
 var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 var mobile = window.innerWidth < 1170;
 
-window.onload = function(e) {
+window.onload = function (e) {
   if (isSafari) {
     document.querySelector("body").classList.add("isIos");
   }
@@ -35,14 +35,11 @@ window.onload = function(e) {
     return;
   };
 
-  input.addEventListener(
-    "keydown",
-    _.debounce(function() {
-      return validateEmail(input.value);
-    }, 2000)
-  );
+  input.addEventListener("keydown", _.debounce(function () {
+    return validateEmail(input.value);
+  }, 2000));
 
-  form.onsubmit = function(e) {
+  form.onsubmit = function (e) {
     e.preventDefault();
     e.stopPropagation();
     validateEmail(input.value);
@@ -51,7 +48,7 @@ window.onload = function(e) {
 
   var showTyped = function showTyped() {
     content.classList.add("is-hidden");
-    content.addEventListener("transitionend", function(e) {
+    content.addEventListener("transitionend", function (e) {
       e.target.style.display = "none";
       document.querySelector("#typed").style.display = "block";
       var typed = new Typed("#typed", {
@@ -63,7 +60,7 @@ window.onload = function(e) {
     });
   };
 
-  img.onclick = function() {
+  img.onclick = function () {
     showTyped();
   };
 };
@@ -81,7 +78,10 @@ var sendData = function sendData(email) {
       authorization: "apikey d4e83080955451a61de5fda1e8295b55-us3"
     },
     body: JSON.stringify(data)
-  }).then(function(response) {
+  }).then(function (response) {
     return response.json();
   });
-};
+}; // desktop move circle 1 vw
+// fonts
+// mobile move circle left+top
+// move texts by 25px to the bottom
