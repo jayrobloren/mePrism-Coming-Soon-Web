c40c6dff4c84eed8c997cb36811f5c0f1267c888
Bohdan Denysiuk <kotuchi0203@gmail.com>
2019-08-07 20:34:44 +0300

    remove node server

--- nodejs/src/middlewares/cors.ts
+++
@@ -1,9 +0,0 @@
-import * as cors from "cors";
-
-const whitelist = ["localhost:3000", "https://meprism-promo.firebaseapp.com"];
-
-const corsOptions = {
-  origin: whitelist
-};
-
-export const corsMiddleware = cors(corsOptions);
